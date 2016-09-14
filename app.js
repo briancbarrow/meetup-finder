2a4a477382221840e499d9a6ae8eb94905102fcb
Brian Barrow <briancbarrow@gmail.com>
2016-09-14 05:26:05 -0600

    basic google map api functionality

--- app.js
+++ app.js
@@ -1,21 +1,55 @@
-$(document).ready(function(){
-  var getQuote = function() {
-    $.ajax({
-      url: 'https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?',
-      dataType: 'jsonp',
-      type: 'GET',
-      success: function(data){
-        $('.quote').text(data.quoteText)
-        if(data.quoteAuthor === ''){
-          $('.author').text('- Anonymous') 
-        } else {
-          $('.author').text('- ' + data.quoteAuthor)
-        }        
+// $(document).ready(function(){
+//     $.ajax({
+//       url: 'http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02',
+//       // data: {
+//       // },
+//       dataType: 'jsonp',
+//       type: 'GET',
+//       success: function(data){
+//         console.log(data);
+//       },
+//       error: function(error){
+//         console.log(error);
+//       }
+//     });
+//     $.getJSON('http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02',
+//      function( response ) {
+//       console.log(response);
+//      })
+// });
+
+var locations = [
+  {name: 'Red', lat: 40.7608, lng: -111.8910},
+  {name: 'Green', lat: 40.9608, lng: -111.8910},
+  {name: 'Blue', lat: 40.5608, lng: -111.8910},
+]
+
+ var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 40.7608, lng: -111.8910},
+          zoom: 8
+        });
+         var marker = new google.maps.Marker({
+          position: {lat: 40.7608, lng: -112.0910},
+          map: map,
+          title: 'Salt Lake'
+        });
+         for(var i in locations){
+          var marker = new google.maps.Marker(
+            {
+              position: {lat: locations[i].lat, lng: locations[i].lng},
+              map: map,
+              title: locations[i].name,
+              clickable: true
+            }
+          );
+          marker.info = new google.maps.InfoWindow({
+            content: locations[i].name
+          });
+          google.maps.event.addListener(marker, 'click', function() {
+            var marker_map = this.getMap();
+            this.info.open(map, this);
+          });
+         };
       }
-    });
-  }
-  $('button').click(function(e){
-    getQuote();
-  });
-  getQuote();
-});
