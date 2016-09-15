8bb760674bfee5b983b27418192dd4f5655dfe59
Brian Barrow <briancbarrow@gmail.com>
2016-09-14 21:26:58 -0600

    complete functionality

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+$(document).ready(function(){
+  var search = function(userInput, location) {
+    var searchValue = 'javascript';
+    var locationVal = '';
+    if(userInput){
+      searchValue = userInput;
+    }   
+    if(location){      
+      locationVal = location;
+      console.log(locationVal)
+    }
     $.ajax({
       url: 'https://api.meetup.com/find/groups',
       data: {
         key: '106175216e3c4ad233449412e3542b',
-        location: '',
-        text: 'javascript',
+        location: locationVal,
+        text: searchValue,
         upcoming_events: true,
         radius: 20
       },
@@ -11,122 +22,64 @@
       type: 'GET',
       success: function(data){
         console.log(data);
+        initMap(data.data, locationVal);
       },
       error: function(error){
         console.log(error);
       }
     });
-
- //  var locations = [
- //    {name: 'Red', lat: 40.7608, lng: -111.8910},
- //    {name: 'Green', lat: 40.9608, lng: -111.8910},
- //    {name: 'Blue', lat: 40.5608, lng: -111.8910},
- //  ]
-
-
-
- // var map;
- //  function initMap() {
- //    map = new google.maps.Map(document.getElementById('map'), {
- //      center: {lat: 41.7608, lng: -111.8910},
- //      zoom: 8
- //    });
- //    console.log(map);
- //     var marker = new google.maps.Marker({
- //      position: {lat: 40.7608, lng: -112.0910},
- //      map: map,
- //      title: 'Salt Lake'
- //    });
- //     // if (navigator.geolocation) {
- //     //      navigator.geolocation.getCurrentPosition(function(position) {
- //     //        var pos = {
- //     //          lat: position.coords.latitude,
- //     //          lng: position.coords.longitude
- //     //        };
-
- //     //        infoWindow.setPosition(pos);
- //     //        infoWindow.setContent('Location found.');
- //     //        map.setCenter(pos);
- //     //      }, function() {
- //     //        handleLocationError(true, infoWindow, map.getCenter());
- //     //      });
- //     //    } else {
- //     //      // Browser doesn't support Geolocation
- //     //      handleLocationError(false, infoWindow, map.getCenter());
- //     //    }
- //     for(var i in locations){
- //      var marker = new google.maps.Marker(
- //        {
- //          position: {lat: locations[i].lat, lng: locations[i].lng},
- //          map: map,
- //          title: locations[i].name,
- //          clickable: true
- //        }
- //      );
- //      marker.info = new google.maps.InfoWindow({
- //        content: locations[i].name
- //      });
- //      google.maps.event.addListener(marker, 'click', function() {
- //        var marker_map = this.getMap();
- //        this.info.open(map, this);
- //      });
- //     };
- //  }
- //  google.maps.event.addDomListener(window, 'load', initMap);
- //  function centerMap(){
-    
- //  }
- //  $('button').click(function(){
- //    console.log(map);
- //    map.setCenter({lat: 40.7608, lng: -112.0910})
- //  })
-
- var map;
-function initialize(){
-  var centerButton = document.getElementById('center')
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: new google.maps.LatLng(41.7608,-111.8910),//Setting Initial Position
-    zoom: 10
-  });
-  var infoWindow = new google.maps.InfoWindow({map: map});
-
-  $('#center').click(function(){
-    console.log(map);
-    map.setCenter({lat: 48.1293954, lng: -112.0910})
-  })
-
-  if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-}
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-      }
-
-function newLocation(newLat,newLng){
-    console.log('clicked')
-    map.setCenter({
-      lat : newLat,
-      lng : newLng
-    });
   }
 
+    function initMap(data, location) {
+      var geocoder = new google.maps.Geocoder();
+      var meetups = [
+        {name: 'Red', lat: 40.7608, lng: -111.8910},
+        {name: 'Green', lat: 40.9608, lng: -111.8910},
+        {name: 'Blue', lat: 40.5608, lng: -111.8910},
+      ]
+      meetups = data;
+      // console.log(meetups[0].lat);
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 40.7608, lng: -111.8910},
+        zoom: 9
+      });
+      var address = location;
+      geocoder.geocode({
+        'address': address
+      }, function(results, status) {
+        if(status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+      } else {
+        console.log('Error reason: ' + status);
+      }
+      })
+       
+   for(var i in meetups){
+    var marker = new google.maps.Marker({
+        position: {lat: meetups[i].lat, lng: meetups[i].lon},
+        map: map,
+        title: meetups[i].name,
+        clickable: true
+      }
+    );
+      marker.info = new google.maps.InfoWindow({
+        content: '<h3>' + meetups[i].name + '</h3>' +
+        '<p>' + meetups[i].description + '</p>' +
+        '<div><a href="' + meetups[i].link + '">' + meetups[i].link + '</div>'
+      });
+      google.maps.event.addListener(marker, 'click', function() {
+        var marker_map = this.getMap();
+        this.info.open(map, this);
+      });
+     };
+    }
 
-  
+  $('form').submit(function(){
+    console.log('Submitted')
+    var input = $('#search-term').val();
+    var location = $('#location').val();
+    search(input, location);
+    return false;
+  });
+  search();
+});
